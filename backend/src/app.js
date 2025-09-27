d127024294a0eea31819b0c92ea2b0af4aa7e5f5
joaoluan <joao.luan@hotmail.com>
2025-09-27 14:44:08 -0300

    atualizacao back e servidor front com vite

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,13 +1,17 @@
 // 1. Importar o módulo Express
 const express = require('express');
-
+// Importa o middleware CORS
+const cors = require('cors'); 
 // 2. Criar uma instância do aplicativo Express
 const app = express();
+app.use(express.json());// Permite que o Express entenda JSON no corpo das requisições
 
 // 3. Definir a porta em que o servidor vai rodar
 // Usamos 3000 ou 3001 para o backend. O frontend geralmente roda na 8080 ou 5173.
+app.use(cors({
+    origin: 'http://localhost:5173' // Substitua pela URL do seu frontend
+}));
 const PORT = 3001;
-
 // 4. Criar uma rota de teste para ver se tudo está funcionando
 // Quando alguém acessar a rota principal ("/") do nosso backend...
 app.get('/', (req, res) => {
